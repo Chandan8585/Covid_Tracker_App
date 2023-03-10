00371bf7ba23b0c8d42eed5a433f057014c8d7c5
Chandan8585 <chandanwingshr@gmail.com>
2023-03-11 01:08:05 +0530

    Project completed but UI designing is need to be done to take this project to next level

--- covidtracker/src/components/covid.js
+++ covidtracker/src/components/covid.js
@@ -1,22 +1,78 @@
-import React, { useEffect } from 'react'
-
+import React, { useEffect, useState } from 'react'
+import './covid.css'
 const Covid = () => {
+const [data, setData] = useState([]);
+
     const getCovidData = async() => {
         try{
             const res = await fetch('https://data.covid19india.org/data.json');
-            console.log(res);
+         const actualData =  await res.json();
+         console.log(actualData.statewise[0]);
+         setData(actualData.statewise[0]);
         } catch (err){
                console.log(err);
         }
       
     }
     useEffect(()=>{
-       getCovidData();
+    getCovidData();
     }, []);
   return (
-    <div>
+    <div className='Main-div'>
+    <div className="header">
       <h1>Live</h1>
       <h2>Covid-19 CORONAVIRUS TRACKER</h2>
+      </div>
+      <section>
+      <ul className='card'>
+      <li>
+      <div className="styleCardContent">
+      <p className="styleCardTitle">TOtal </p>
+      <p className="styleLocationLabel">Recoverd Cases</p>
+      <p className="styleDescription">{data.recovered}</p>
+    </div>
+    </li>
+    <li>
+      <div className="styleCardContent">
+      <p className="styleCardTitle">Country Name</p>
+      <p className="styleLocationLabel">India</p>
+      <p className="styleDescription"></p>
+    </div>
+      </li>
+    <li>
+    <div className="styleCardContent">
+      <p className="styleCardTitle">Total </p>
+      <p className="styleLocationLabel">Deaths</p>
+      <p className="styleDescription">{data.deaths}</p>
+    </div>
+      </li>
+       
+      <li>
+      <div className="styleCardContent">
+      <p className="styleCardTitle">Total </p>
+      <p className="styleLocationLabel">Active Cases</p>
+      <p className="styleDescription">{data.active}</p>
+    </div>
+      </li>
+      <li>
+      <div className="styleCardContent">
+      <p className="styleCardTitle">Total </p>
+      <p className="styleLocationLabel">Confirmed Cases</p>
+      <p className="styleDescription">{data.confirmed}</p>
+    </div>
+    </li>
+      <li>
+      <div className="styleCardContent">
+      <p className="styleCardTitle">Last </p>
+      <p className="styleLocationLabel">Update Time</p>
+      <p className="styleDescription">{data.lastupdatedtime}</p>
+    </div>
+      </li>
+      </ul>
+      </section>
+      <div className="footer">
+      <p>CopyRight @ Chandan Pratap</p>
+      </div>
     </div>
   )
 }
